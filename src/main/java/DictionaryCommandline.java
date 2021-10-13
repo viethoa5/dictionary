91244e54852529fcab22148200c80e3bda3e0132
Nguyễn Tuấn Anh <nguyentuananh@MacBook-Pro-cua-Nguyen.local>
2021-10-13 15:16:10 +0700

    version 2.1

--- src/main/java/DictionaryCommandline.java
+++ src/main/java/DictionaryCommandline.java
@@ -6,15 +6,17 @@ import java.util.Scanner;
 
 public class DictionaryCommandline {
     DictionaryManagement manage = new DictionaryManagement();
+
     public void showAllWords() {
         System.out.println("No   | English     | Vietnamese");
-        for(int i = 0; i < Dictionary.size; i++) {
+        for(int i = 0; i < Dictionary.wordArrays.size(); i++) {
                 System.out.print(i+1);
                 System.out.print("   | " + StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_target()) +"       | ");
                 System.out.print(StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_expland()));
                 System.out.println();
         }
     }
+
     public void dictionaryBasic() {
         manage.insertFromCommandline();
         showAllWords();
