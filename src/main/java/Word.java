dfa9d6b0aeb60ee1a1313ce55a1bebe796025ade
Nguyễn Tuấn Anh <nguyentuananh@MacBook-Pro-cua-Nguyen.local>
2021-10-13 21:28:43 +0700

    version 3

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,13 +1,15 @@
 public class Word {
         private String word_target;
         private String word_explain;
+
         public Word() {
             this.word_target = "";
             this.word_explain = "";
         }
-        public Word(String word_target, String word_expland) {
+
+        public Word(String word_target, String word_explain) {
             this.word_target = word_target;
-            this.word_explain = word_expland;
+            this.word_explain = word_explain;
         }
 
         public String getWord_explain() {
@@ -19,8 +21,8 @@ public class Word {
         }
 
         public void setWord_explain(String word_explain) {
-               this.word_explain = word_explain;
-           }
+            this.word_explain = word_explain;
+        }
 
          public void setWord_target(String word_target) {
             this.word_target = word_target;
