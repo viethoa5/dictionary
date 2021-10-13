26773b593f9feea0a31924715e02407300f6918c
viethoa5 <viethoa5555@gmail.com>
2021-10-13 16:34:43 +0700

    version 2.2

--- src/main/java/DictionaryCommandline.java
+++ src/main/java/DictionaryCommandline.java
@@ -6,25 +6,20 @@ import java.util.Scanner;
 
 public class DictionaryCommandline {
     DictionaryManagement manage = new DictionaryManagement();
+
     public void showAllWords() {
         System.out.println("\t\t\t\tNo\t|" + "English" + "\t\t\t  | VietNamese ");
-        for(int i = 0; i < Dictionary.size; i++) {
-                System.out.print("\t\t\t\t"+(i+1)+"\t| ");
-                System.out.printf("%-20s| ", StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_target()));
-                System.out.print( StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_explain()));
-                System.out.println();
+        for (int i = 0; i < Dictionary.size; i++) {
+            System.out.print("\t\t\t\t" + (i + 1) + "\t| ");
+            System.out.printf("%-20s| ", StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_target()));
+            System.out.print(StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_explain()));
+            System.out.println();
         }
     }
+
     public void dictionaryBasic() {
         manage.insertFromCommandline();
         showAllWords();
     }
-
-    public void dictionaryAdvanced() throws IOException {
-        this.manage.insertFromFile();
-        this.showAllWords();
-        Scanner sc = new Scanner(System.in);
-        String tar = sc.next().toLowerCase();
-        this.manage.dictionaryLookup(tar);
-    }
 }
+
--- src/main/java/DictionaryManagement.java
+++ src/main/java/DictionaryManagement.java
@@ -16,29 +16,8 @@ public class DictionaryManagement {
             String define = scan.nextLine();
             System.out.println("Word explain :");
             String meaning = scan.nextLine();
-            Word words    = new Word(define,meaning);
+            Word words = new Word(define, meaning);
             Dictionary.wordArrays.add(words);
         }
-    }
-    public void insertFromFile() throws IOException {
-        Scanner scanner = new Scanner(Paths.get("main/dictionaries.txt"), "UTF-8");
-
-        while (scanner.hasNext()) {
-            while (scanner.hasNextLine()) {
-                Word x = new Word();
-                String s = scanner.nextLine();
-                String[] word = s.split("\\t", 2);
-                x.setWord_target(word[0]);
-                x.setWord_expland(word[1]);
-                Dictionary.wordArrays.add(x);
-            }
-        }
-    }
-
-    public boolean dictionaryLookup(String tar) {
-        Word a = (Word)Dictionary.wordArrays.stream().filter((w) -> {
-            return tar.equals(w.getWord_target());
-        }).findFirst().orElse(null);
-        return a != null;
     }
 }
