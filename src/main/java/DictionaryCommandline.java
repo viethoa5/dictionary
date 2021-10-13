40efd6af8e2e470cc5d9f199a8f50b7d4afe9cea
19020355-ledanglong <91559403+19020355-ledanglong@users.noreply.github.com>
2021-10-13 18:06:48 +0700

    version 3.1

--- src/main/java/DictionaryCommandline.java
+++ src/main/java/DictionaryCommandline.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
 import org.apache.commons.lang3.StringUtils;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -8,12 +11,12 @@ public class DictionaryCommandline {
     DictionaryManagement manage = new DictionaryManagement();
 
     public void showAllWords() {
-        System.out.println("No   | English     | Vietnamese");
-        for(int i = 0; i < Dictionary.wordArrays.size(); i++) {
-                System.out.print(i+1);
-                System.out.print("   | " + StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_target()) +"       | ");
-                System.out.print(StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_expland()));
-                System.out.println();
+        System.out.println("\t\t\t\tNo\t|" + "English" + "\t\t\t  | VietNamese ");
+        for(int i = 0; i < Dictionary.size; i++) {
+            System.out.print("\t\t\t\t"+(i+1)+"\t| ");
+            System.out.printf("%-20s| ", StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_target()));
+            System.out.print( StringUtils.capitalize(Dictionary.wordArrays.get(i).getWord_expland()));
+            System.out.println();
         }
     }
 
@@ -22,11 +25,38 @@ public class DictionaryCommandline {
         showAllWords();
     }
 
+    public void dictionarySearcher() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập từ cần tìm: ");
+        String wordSearch = sc.next();
+        wordSearch.toLowerCase();
+        try {
+            File inFile = new File("dictionaries.txt");
+            FileReader fileReader = new FileReader(inFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith(wordSearch)) {
+                    System.out.println(line);
+                }
+            }
+            reader.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void dictionaryAdvanced() throws IOException {
         this.manage.insertFromFile();
         this.showAllWords();
         Scanner sc = new Scanner(System.in);
         String tar = sc.next().toLowerCase();
         this.manage.dictionaryLookup(tar);
+        this.manage.addWord();
+        this.manage.deleteWord();
+        this.manage.editWord();
+        this.manage.dictionaryExportToFile();
+        dictionarySearcher();
     }
 }
--- src/main/java/DictionaryManagement.java
+++ src/main/java/DictionaryManagement.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.io.IOException;
 import java.util.Scanner;
 import java.nio.file.Paths;
@@ -17,8 +18,9 @@ public class DictionaryManagement {
             Dictionary.wordArrays.add(words);
         }
     }
+
     public void insertFromFile() throws IOException {
-        Scanner scanner = new Scanner(Paths.get("src/main/dictionaries.txt"), "UTF-8");
+        Scanner scanner = new Scanner(Paths.get("main/dictionaries.txt"), "UTF-8");
 
         while (scanner.hasNext()) {
             while (scanner.hasNextLine()) {
@@ -36,7 +38,52 @@ public class DictionaryManagement {
         Word a = (Word)Dictionary.wordArrays.stream().filter((w) -> {
             return tar.equals(w.getWord_target());
         }).findFirst().orElse(null);
-        System.out.println(a.getWord_target() + ":  " + a.getWord_expland());
         return a != null;
+    }
+
+    public void dictionaryExportToFile() {
+        try {
+            FileWriter writer = new FileWriter("dictionaries.txt");
+            for (int i = 0; i < Dictionary.wordArrays.size(); i++) {
+                writer.write(Dictionary.wordArrays.get(i).getWord_target() + " " + Dictionary.wordArrays.get(i).getWord_expland() + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addWord() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập từ cần thêm vào (tiếng Anh): ");
+        String word_target = sc.next();
+        System.out.print("Nhập nghĩa từ mới: ");
+        String word_explain = sc.nextLine();
+        Word newWord = new Word(word_target, word_explain);
+        Dictionary.wordArrays.add(newWord);
+    }
+
+    public void deleteWord() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập từ cần xóa (tiếng Anh): ");
+        String wordDelete = sc.next();
+        for(int i = 0; i < Dictionary.wordArrays.size(); i++) {
+            if(Dictionary.wordArrays.get(i).getWord_target().equals(wordDelete)) {
+                Dictionary.wordArrays.remove(i);
+            }
+        }
+    }
+
+    public void editWord() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập từ cần sửa(tiếng Anh): ");
+        String wordEdit = sc.next();
+        for(int i = 0; i < Dictionary.wordArrays.size(); i++) {
+            if(Dictionary.wordArrays.get(i).getWord_target().equals(wordEdit)) {
+                System.out.print("Nhập nghĩa mong muốn: ");
+                String new_target = sc.nextLine();
+                Dictionary.wordArrays.get(i).setWord_expland(new_target);
+            }
+        }
     }
 }
