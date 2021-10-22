7c09399aa8a73b6abebcfedfb3e4ba07b43a6100
viethoa5 <viethoa5555@gmail.com>
2021-10-22 15:55:34 +0700

    'version4.7'

---
+++ src/main/java/javaController/DeleteControll.java
@@ -0,0 +1,17 @@
+package javaController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+
+public class DeleteControll {
+    @FXML
+    public Button choice1;
+
+    @FXML
+    public Button choice2;
+
+
+}
+
